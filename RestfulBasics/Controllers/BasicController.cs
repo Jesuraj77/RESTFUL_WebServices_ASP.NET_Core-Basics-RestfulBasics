f3655da349fcd540badae0e94f9fb1ec4bb7096d
itdev-lenovo <it1devops@gmail.com>
2020-03-26 20:26:35 +0530

    files added

---
+++ RestfulBasics/Controllers/BasicController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestfulBasics.Controllers
+{
+    public class BasicController : Controller
+    {
+       // Create a simple "Hello World" Program and execute the same using CLI Command.
+    }
+}
